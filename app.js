c99211c0c146fdf206bc3e3b2d532ce463ab588b
MrCsabaToth <csaba.toth.us@gmail.com>
2017-03-19 08:58:35 -0700

    Sort the result lexically, case insensitive

--- app.js
+++ app.js
@@ -40,9 +40,18 @@ app.get('/process', function(req, res) {
       participants.push(part);
     }
   });
-  var partSorted = participants.sort(function(a, b) { return b.rank - a.rank; });
+  participants.sort(
+    function(a, b) {
+      var nameA = a.name.toLowerCase(), nameB = b.name.toLowerCase();
+      if (nameA < nameB) //sort string ascending
+        return -1;
+      if (nameA > nameB)
+        return 1;
+      return 0; //default return value (no sorting)
+    }
+  );
 
-  fs.writeFile('raffleFeed.jsonp', 'participants=' + JSON.stringify(partSorted, null, 2), function (err) {
+  fs.writeFile('raffleFeed.jsonp', 'participants=' + JSON.stringify(participants, null, 2), function (err) {
     if (err) {
       res.send(err);
     } else {
