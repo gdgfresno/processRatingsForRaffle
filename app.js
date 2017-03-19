22b5698572719a859f62032a53267e88515b48d1
MrCsabaToth <csaba.toth.us@gmail.com>
2017-03-18 22:37:09 -0700

    Rating precessing code

---
+++ app.js
@@ -0,0 +1,58 @@
+/*eslint-env node*/
+
+//------------------------------------------------------------------------------
+// node.js starter application
+//------------------------------------------------------------------------------
+
+// This application uses express as its web server
+// for more info, see: http://expressjs.com
+var express = require('express');
+
+// create a new express server
+var app = express();
+
+// serve the files out of ./public as our main files
+app.use(express.static(__dirname + '/public'));
+
+app.get('/process', function(req, res) {
+  var fs = require('fs');
+  var ratingsText = fs.readFileSync('ratings.json', 'utf8');
+  var ratingsJson = JSON.parse(ratingsText);
+
+  var participants = [];
+  Object.keys(ratingsJson).forEach(function(key) {
+    var participant = ratingsJson[key];
+    if (participant.email) {
+      var generalSurveyCompleted = false;
+      var sessionSurveyCompleted = 0;
+      if (participant.sessions) {
+        var sessionKeys = Object.keys(participant.sessions);
+        var numSession = sessionKeys.length;
+        generalSurveyCompleted = sessionKeys.indexOf('137') >= 0;
+        sessionSurveyCompleted = generalSurveyCompleted ? numSession - 1 : numSession;
+      }
+      var part = {
+        'email': participant.email,
+        'name': participant.name || '',
+        'generalSurvey': generalSurveyCompleted,
+        'sessionSurvey': sessionSurveyCompleted
+      }
+      participants.push(part);
+    }
+  });
+  var partSorted = participants.sort(function(a, b) { return b.rank - a.rank; });
+
+  fs.writeFile('raffleFeed.jsonp', 'participants=' + JSON.stringify(partSorted, null, 2), function (err) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send('Processing finished');
+    }
+  });
+});
+
+// Start server on the specified port and binding host
+app.listen(5858, '0.0.0.0', function() {
+  // Print a message when the server starts listening
+  console.log('server starting on 5858');
+});
